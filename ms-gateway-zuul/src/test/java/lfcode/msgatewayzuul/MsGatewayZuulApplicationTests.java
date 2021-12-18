dea316ae0f151bc44ea31b478b058ddf0215bba1
Fernando Oliveira <47955596+lfpo2005@users.noreply.github.com>
2021-12-18 15:17:44 -0300

    Project ms-gateway-zuul created

---
+++ ms-gateway-zuul/src/test/java/lfcode/msgatewayzuul/MsGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package lfcode.msgatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
