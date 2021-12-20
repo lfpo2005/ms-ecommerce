1af23230a6a9dfa446f0bcacd8c6f699103f8bd7
Fernando Oliveira <47955596+lfpo2005@users.noreply.github.com>
2021-12-20 20:24:03 -0300

    ms-ecommerce-config-server created

---
+++ ms-ecommerce-config-server/src/main/java/com/lfcode/msecommerceconfigserver/MsEcommerceConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.lfcode.msecommerceconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEcommerceConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsEcommerceConfigServerApplication.class, args);
+    }
+
+}
---
+++ ms-ecommerce-config-server/src/test/java/com/lfcode/msecommerceconfigserver/MsEcommerceConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lfcode.msecommerceconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEcommerceConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
