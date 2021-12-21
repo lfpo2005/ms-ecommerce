4da96c4aa1e3cd526ad5ec7af0ba5d5f7c83dc5f
Fernando Oliveira <47955596+lfpo2005@users.noreply.github.com>
2021-12-20 21:04:33 -0300

    ms-ecommerce-config-server config

--- ms-config-server/src/main/java/com/lfcode/msecommerceconfigserver/MsConfigServerApplication.java
+++ ms-config-server/src/main/java/com/lfcode/msecommerceconfigserver/MsConfigServerApplication.java
@@ -2,12 +2,14 @@ package com.lfcode.msecommerceconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
-public class MsEcommerceConfigServerApplication {
+public class MsConfigServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MsEcommerceConfigServerApplication.class, args);
+        SpringApplication.run(MsConfigServerApplication.class, args);
     }
 
 }
